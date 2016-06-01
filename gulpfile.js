b8de34e3ce77b813bf42b7a688ba187078cb331b
max <gnatkovsky@bigmir.net>
2016-06-01 08:58:09 +0300

    setting

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var less = require('gulp-less');
 var spritesmith = require('gulp.spritesmith');
 var rename = require('gulp-rename');
 var includer = require('gulp-htmlincluder');
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
+
+gulp.task('server', function(){
+	connect.server({
+		root : 'build',
+		livereload: true
+	});
+});
 
 gulp.task('spriteCreator', function(){
 	var sprite = gulp.src('dev/img/icons/*.png').pipe(spritesmith({
@@ -17,8 +26,25 @@ gulp.task('spriteCreator', function(){
 });
 
 gulp.task('cssCreator', function(){
-	gulp.src('dev/less/general.less').pipe(less()).pipe(gulp.dest('build/css'));
-})
+	gulp.src('dev/less/**/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('build/css/'))
+		.pipe(connect.reload());
+});
+
 gulp.task('htmlCreator', function(){
-	gulp.src('dev/**/*.html').pipe(includer()).pipe(gulp.dest('build/'));
+	gulp.src('dev/**/*.html')
+		.pipe(includer())
+		.pipe(gulp.dest('build/'))
+		.pipe(connect.reload());
+});
+gulp.task('default', function(){
+	gulp.start('cssCreator', 'htmlCreator', 'server');
+
+	gulp.watch(['dev/less/**/*.less'], function(){
+		gulp.start('cssCreator');
+	});
+	gulp.watch(['dev/**/*.html'], function(){
+		gulp.start('htmlCreator');
+	});
 });
